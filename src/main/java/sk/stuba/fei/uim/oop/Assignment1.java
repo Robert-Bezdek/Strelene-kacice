40504189c2a892d79ebd4c95c7268fdb79b603ad
xpalenik <xpalenik@gmail.com>
2022-03-21 12:02:57 +0100

    Skusobny commit

--- src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
@@ -2,6 +2,8 @@ package sk.stuba.fei.uim.oop;
 
 import sk.stuba.fei.uim.oop.streleneKacice.StreleneKacice;
 
+// skusobny commit
+
 public class Assignment1 {
     public static void main(String[] args) {
         StreleneKacice streleneKacice = new StreleneKacice();
