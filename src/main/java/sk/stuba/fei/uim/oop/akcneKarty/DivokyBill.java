2bcfb3b3c0da2ef8ada63b011a0666c2e1f4b97f
Martin Páleník <xpalenik@gmail.com>
2022-03-23 17:34:29 +0100

    Oprav kartu Divoky Bill
    Povodne sme uzivatela samostatne ziadali o poziciu pre zamierenie a samostatne pre strielanie. Musi to byt obe naraz.

--- src/main/java/sk/stuba/fei/uim/oop/akcneKarty/DivokyBill.java
+++ src/main/java/sk/stuba/fei/uim/oop/akcneKarty/DivokyBill.java
@@ -1,17 +1,32 @@
 package sk.stuba.fei.uim.oop.akcneKarty;
 
 import sk.stuba.fei.uim.oop.BalicekKaciekaVody;
+import sk.stuba.fei.uim.oop.KartyRybnika.Kacica;
 import sk.stuba.fei.uim.oop.Rybnik;
 import sk.stuba.fei.uim.oop.Zameriavac;
+import sk.stuba.fei.uim.oop.utility.KeyboardInput;
 
 public class DivokyBill implements AkcneKarty {
     @Override
     public void pouzitKartu(Rybnik r, BalicekKaciekaVody b, Zameriavac z) {
-        Zamierit zamierit = new Zamierit();
-        Vystrelit vystrelit = new Vystrelit();
+        int ciel;
+        do {
+            ciel = KeyboardInput.readInt("Vyber policko, na ktore mieris a strielas: [0-5]");
+            try {
+                z.zamierit(ciel);
+                z.vystrel(ciel);
+                if (r.pozriKartu(ciel) instanceof Kacica){
+                    ((Kacica) r.vyberKartu(ciel)).getPatriHracovi().zasiahni();
+                    r.vlozKartuNaKoniec(b.lizniKartu());
+                }
 
-        zamierit.pouzitKartu(r,b,z);
-        vystrelit.pouzitKartu(r,b,z);
+                break;
+            } catch (IndexOutOfBoundsException e){
+                System.out.println("Neplatna pozicia zameriavaca, platne pozicie su 0,1,2,3,4,5.");
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        } while (true);
     }
 
     @Override
